346ab3953f3d660c4655f035c4698ffb4974c4ed
CloudGuan <kirito291305463@gmail.com>
2016-11-03 20:46:32 +0800

    implement timer

--- DXLearnProgram/DXAppTimer.cpp
+++ DXLearnProgram/DXAppTimer.cpp
@@ -1,12 +1,89 @@
-#pragma once
 #include "DXAppTimer.h"
-#include <math.h>
+
 Timer::Timer()
+	:preTime(0), baseTime(0)
+	, pauseTime(0), currentTime(0)
+	, stopTime(0)
+	, secondPerCount(0.0)
+	, bStopped(false)
 {
-	
+	__int64 countPerSceond;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countPerSceond);
+	secondPerCount = 1.0 / (double)countPerSceond;
 }
 
 Timer::~Timer()
 {
 
+}
+
+float Timer::DeltaTime() const
+{
+	return (float)iDeltaTime;
+}
+
+float Timer::TotalTime() const
+{
+	double res;
+	if (bStopped)
+	{
+		res = ((stopTime - pauseTime) - baseTime)*secondPerCount;
+	}
+	else
+	{
+		res = ((currentTime - pauseTime) - baseTime)*secondPerCount;
+	}
+	return (float)res;
+}
+
+void Timer::RestTimer()
+{
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	baseTime = currentTime;
+	preTime = currentTime;
+	bStopped = false;
+	pauseTime = 0;
+}
+
+void Timer::Start()
+{
+	if (bStopped)
+	{
+		QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+		pauseTime += (currentTime-stopTime);
+		preTime = currentTime;
+		stopTime = 0;
+		bStopped = false;
+	}
+}
+
+void Timer::Stop()
+{
+	if (!bStopped)
+	{
+		QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+		stopTime = currentTime;
+		bStopped = true;
+	}
+}
+
+void Timer::Tick()
+{
+	if (bStopped)
+	{
+		iDeltaTime = 0.0;
+		return;
+	}
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	
+	iDeltaTime = (currentTime - preTime)*secondPerCount;
+	preTime = currentTime;
+
+
+	// 确保不为负值。DXSDK中的CDXUTTimer提到：如果处理器进入了节电模式
+	// 或切换到另一个处理器，mDeltaTime会变为负值。
+	if (iDeltaTime<0)
+	{
+		iDeltaTime = 0.0;
+	}
 }
--- DXLearnProgram/DXAppTimer.h
+++ DXLearnProgram/DXAppTimer.h
@@ -1,10 +1,28 @@
 #pragma once 
-
+#include <windows.h>
 
 class Timer
 {
 public:
 	Timer();
-	~Timer();
-public:
+	virtual ~Timer();
+
+	float	DeltaTime()const;
+	float	TotalTime()const;
+
+	void	RestTimer();
+	void	Start();
+	void	Stop();
+	void	Tick();
+private:
+	__int64 preTime;
+	__int64 baseTime;
+	__int64 pauseTime;
+	__int64 currentTime;
+	__int64 stopTime;
+
+	double secondPerCount;
+	double iDeltaTime;
+
+	bool bStopped;
 };
